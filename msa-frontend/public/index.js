7fec16377a541d06af06ee2b860d50eecab78b91
gongbu22 <pyujin0711@naver.com>
2024-10-15 17:14:05 +0900

    msa frontend - index 변경, 회원 조회, CSR(클라이언트사이드렌더링) 추가

--- msa-frontend/public/index.js
+++ msa-frontend/public/index.js
@@ -3,17 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.json({msg: "Hello, World!!"})
-  // html코드를 보여주기위해 res.send 사용
-  res.send('<h1>Hello, World!!</h1>' +
-    '<div><a href="/user">회원가입</a></div>' +
-    '<div><a href="/product">상품등록</a></div>');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 /* user registration */
 router.get('/user', function(req, res, next) {
   // 파일을 보여주기위해 sendFile 사용
   res.sendFile(__dirname + '/views/user.html');
+});
+
+/* user list */
+router.get('/users', function(req, res, next) {
+  // 파일을 보여주기위해 sendFile 사용
+  res.sendFile(__dirname + '/views/users.html');
 });
 
 /* product registration */
