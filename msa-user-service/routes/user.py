3be24a1c8bc497c07cf3dbaf398f713f8cd0aaec
gongbu22 <pyujin0711@naver.com>
2024-10-16 14:57:07 +0900

    user service - 회원 상세 조회 기능 추가

--- msa-user-service/routes/user.py
+++ msa-user-service/routes/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from schema.user import User, UserBase, UserList
 from service.database import get_db
-from service.user import register, userlist
+from service.user import register, userlist, userone
 
 router = APIRouter()
 
@@ -23,3 +23,14 @@ async def list_users(db:Session=Depends(get_db)):
     # pydantic형식(json)에서 orm 형식으로 변환
     # return [UserList.from_orm(u) for u in users]
     return [UserList.model_validate(u) for u in users]
+
+@router.get('/user/{mno}', response_model=User)
+async def user_one(mno: int, db:Session=Depends(get_db)):
+
+    user = userone(db, mno)
+
+    # 테이블 조회한 결과 객체를
+    # UserList 형식의 배열로 재생성
+    # pydantic형식(json)에서 orm 형식으로 변환
+    # return [UserList.from_orm(u) for u in users]
+    return User.model_validate(user)
