41b89e8a2a1bb2e267325e7b4b03f4ef76f298e6
gongbu22 <pyujin0711@naver.com>
2024-10-17 12:17:43 +0900

    user service - 회원 로그인 기능 추가, 로그인 성공시 토큰 생성 추가, 로그인 실패시 401 응답코드 반환

--- msa-user-service/routes/user.py
+++ msa-user-service/routes/user.py
@@ -1,9 +1,10 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from schema.user import User, UserBase, UserList
+from schema.user import User, UserBase, UserList, Token, UserLogin
+from service.auth import userlogin
 from service.database import get_db
 from service.user import register, userlist, userone
 
@@ -31,8 +32,22 @@ async def user_one(mno: int, db:Session=Depends(get_db)):
 
     user = userone(db, mno)
 
+    if user is None:
+        raise HTTPException(404, 'User not found!!')
+
     # 테이블 조회한 결과 객체를
     # UserList 형식의 배열로 재생성
     # pydantic형식(json)에서 orm 형식으로 변환
     # return [UserList.from_orm(u) for u in users]
     return User.model_validate(user)
+
+@router.post('/userlogin', response_model=Optional[Token])
+async def user_login(login: UserLogin, db:Session=Depends(get_db)):
+
+    token = userlogin(login, db)
+    print(token)
+
+    if token is None:
+        raise HTTPException(401, '로그인 실패! - 아이디나 비밀번호가 틀려요!')
+
+    return token
