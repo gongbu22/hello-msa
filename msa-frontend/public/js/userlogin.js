e2dd3b508ef55bbb0af218077308cdfe80b70165
gongbu22 <pyujin0711@naver.com>
2024-10-17 15:58:39 +0900

    frontend - 로그인 후 토큰 확인, 세션스토리지에 토큰 저장

--- msa-frontend/public/js/userlogin.js
+++ msa-frontend/public/js/userlogin.js
@@ -17,10 +17,22 @@ loginbtn.addEventListener('click', () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(jsondata)
-        })
-        .then(res => {
-            if (res.status === 401) {alert('회원 로그인 실패!!');}
-            else if (res.status === 200) {alert('회원 로그인 성공!!');}
+        }).then(res => {
+            if (res.status === 401) {
+                alert('회원 로그인 실패!!');
+            } else if (res.status === 200) {
+                return res.json();  // 밑에 data
+            }
+        }).then(data => {
+            if (data) {
+                // console.log(data.access_token); // 토큰 확인
+                // javascript 내에 저장소가 있음 localStorage
+                // localStorage.setItem('token', data.access_token); // 토큰 저장
+                sessionStorage.setItem('token', data.access_token);
+                alert('회원 로그인 성공!!');
+            } else {
+                alert('로그인 토큰 확인 불가!!');
+            }
         }).catch((error) => {
             alert('회원 로그인 오류 발생!!');
         });
