3ee74d8d17bbac85e2f62e3eada16a38fd53b850
gongbu22 <pyujin0711@naver.com>
2024-10-18 14:58:25 +0900

    frontend - axios, session 패키지 설치, 서버측 세션 기능 추가, 세션여부에 따라 secure 페이지 접근 제어, 로그아웃 기능 추가

--- msa-frontend/app.js
+++ msa-frontend/app.js
@@ -1,13 +1,22 @@
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
-let request = require('request');
+// nodejs에서 제공하는 세션관리 패키지
+let session = require('express-session');
 let port = 3000;
 
 let indexRouter = require('./public/index');
 let naverRouter = require('./public/naver');
 
 let app = express();
+
+// session 설정
+// resave, saveUninitialized - 로그인하지 않은 클라이언트의 세션은 저장 X
+app.use(session({
+  secret: 'Hello, World!!',  // 세션 데이터 암호화시 사용하는 비밀키
+  resave: false,  // 세션 데이터 수정시 재저장 여부
+  saveUninitialized: false  // 세션 초기화 관련 설정, 사용자가 세션을 실제로 사용할 때만 세션이 저장되도록 함
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
