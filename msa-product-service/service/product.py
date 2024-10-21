ef47e914b64cdf0944cd4550284e04dc6e747d0c
gongbu22 <pyujin0711@naver.com>
2024-10-21 11:20:14 +0900

    product service - 상품 수정 기능 추가

--- msa-product-service/service/product.py
+++ msa-product-service/service/product.py
@@ -37,3 +37,28 @@ def productdelete(db:Session, pno: int):
     # 만일, 프로그래밍으로 삭제한 상품수를 return 하려면
     # core orm을 이용할 것! (db.execute(delete~))
     return 1
+
+# 상품 수정
+def productupdate(db:Session, product: Product):
+    # 삭제할 상품 조회
+    productone = db.query(Product).filter(Product.pno == product.pno).first()
+
+    # setattr(대상, 키, 값) : 실행 중에 특정 객체의 키를 기준으로 값을 수정함
+    # 특정 객체의 속성들을 반복적으로 처리할 때 주로 사용
+    if productone: # 삭제할 상품이 존재한다면
+        # productone.name = product.name
+        # productone.desc = product.desc
+        # productone.price = product.price
+        # productone.maker = product.maker
+        # productone.regdate = product.regdate
+
+        for key, val in product.__dict__.items():
+            if key != 'pno' and val is not None:
+                setattr(productone, key, val)
+        db.commit()
+
+    else:
+        return None
+
+    # 수정한 상품수를 직접 return 함
+    return 1
